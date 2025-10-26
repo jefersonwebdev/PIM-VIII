6ad031dd86f22dae08711e26d43f49fd1fec440f
Jeferson Guimarães <jeferson.phelipe@gmail.com>
2025-10-26 17:29:02 -0300

    Adicionar arquivos de projeto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PIM_VIII
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
